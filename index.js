bc542eb793b55470c867a3e21967665e8c59b37d
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-03-11 16:16:08 +0500

    as professional

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { Telegraf } = require('telegraf');
+const express = require('express');
+const { config } = require('dotenv');
 const { help } = require('./src/config.help.js');
 const { faq } = require('./src/faq.js');
 
-const bot = new Telegraf('6220957060:AAFfU6r3es8EH6hh5zTmMvre1A-He8NyQnE');
+config();
+const app = express();
+
+app.use(express.json());
+
+const bot = new Telegraf(process.env.TOKEN);
 
 help(bot)
 faq(bot)
@@ -11,4 +18,15 @@ bot.action('not_found', ctx => {
     ctx.answerCbQuery('Hali mavjud emas!')
 })
 
+app.get('/', (req,res) => {
+    res.status(200).json({
+        status: "OK",
+        server: "ON"
+    })
+})
+
 // bot.launch();
+
+app.listen(5000, () => {
+    console.log('server running')
+})
